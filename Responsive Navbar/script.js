8ee99eaa5715d5945de32615c67b9be0229d1327
bhargav-borah <100614703+bhargav-borah@users.noreply.github.com>
2022-10-17 12:24:03 +0530

    Update script.js

--- Responsive Navbar/script.js
+++ Responsive Navbar/script.js
@@ -1,1 +1,8 @@
+const toggleButton = document.querySelector(".toggle-button");
+const navbarLinks = document.querySelector(".navbar-links");
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+    console.log("Done");
+})
 
